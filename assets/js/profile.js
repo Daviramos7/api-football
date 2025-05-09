568dfe894e8d2d9f8974eb89f448b07ca1b2cbbe
Luiz Gabriel <lgabrielsantiago09@gmail.com>
2025-05-09 19:26:19 -0300

    Adicionado profile.js

---
+++ assets/js/profile.js
@@ -0,0 +1,26 @@
+// Verifica se o usuário está logado
+const user = JSON.parse(localStorage.getItem('session'));
+if (!user) {
+    alert('Você precisa estar logado!');
+    window.location.href = 'index.html';
+} else {
+    document.getElementById('userName').innerText = `Nome: ${user.name}`;
+    document.getElementById('userEmail').innerText = `E-mail: ${user.email}`;
+}
+
+// Logout do usuário
+document.getElementById('logout').addEventListener('click', () => {
+    localStorage.removeItem('session');
+    window.location.href = 'index.html';
+});
+
+const links = document.querySelectorAll('nav ul li a');
+const currentPage = window.location.pathname.split('/').pop();
+
+links.forEach(link => {
+  const linkHref = link.getAttribute('href');
+
+  if (linkHref === currentPage) {
+    link.classList.add('active');
+  }
+});
